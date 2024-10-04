62b700f29d68d19f2e741368b4ab541d6afc1a41
Heptazhou <zhou@0h7z.com>
2024-10-04 23:56:58 +0000

    Delete

--- src/css/md.js
+++
@@ -1,9 +0,0 @@
-// import "@/css/md"
-
-// https://github.com/PrismJS/prism-themes
-// 1 box
-import "prism-themes/themes/prism-cb.css"
-// 2 font
-import "prism-themes/themes/prism-night-owl.css"
-// 3 color
-import "prism-themes/themes/prism-coldark-dark.css"
