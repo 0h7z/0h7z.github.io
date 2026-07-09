a2da7b25c38e7f9b2b121847c4286084584b3d16
Heptazhou <zhou@0h7z.com>
2026-07-09 04:19:39 +0000

    Build

---
+++ docs/@/en_about_index.md.abuh18rcz6.js
@@ -0,0 +1,6 @@
+import{O as e,_ as t,h as n,tt as r,u as i}from"./~/runtime-core.esm-bundler.f9d2schnhc.js"
+import{n as a}from"./~/theme-default.g2mc4nj9dh.js"
+import{n as o,t as s}from"./~/about.mwfuh46a9k.js"
+var c=JSON.parse(`{"title":"","description":"","frontmatter":{},"headers":[],"relativePath":"en/about/index.md","filePath":"about/index@en.md","lastUpdated":1748717929000}`)
+var l=t({name:`en/about/index.md`,setup(t){return(t,c)=>(e(),i(`div`,null,[n(r(a),{size:`medium`,members:r(s)(`en`)},null,8,[`members`]),n(r(a),{size:`small`,members:r(o)(`en`)},null,8,[`members`])]))}})
+export{c as __pageData,l as default}
--- docs/@/zh_about_index.md.j4ia1p70yq.js
+++
@@ -1,5 +0,0 @@
-import{O as e,_ as t,h as n,tt as r,u as i}from"./~/runtime-core.esm-bundler.f9d2schnhc.js"
-import{n as a}from"./~/theme-default.om5vzn8cp1.js"
-import{n as o,t as s}from"./~/about.mwfuh46a9k.js"
-var c=JSON.parse(`{"title":"","description":"","frontmatter":{},"headers":[],"relativePath":"zh/about/index.md","filePath":"about/index@zh.md","lastUpdated":1748717929000}`),l=t({name:`zh/about/index.md`,setup(t){return(t,c)=>(e(),i(`div`,null,[n(r(a),{size:`medium`,members:r(s)(`zh`)},null,8,[`members`]),n(r(a),{size:`small`,members:r(o)(`zh`)},null,8,[`members`])]))}})
-export{c as __pageData,l as default}
